d6840d6d0c17ae0e41bb6c989ec15e350a8684d2
hwang-eungyeong <yhr05008@naver.com>
2025-06-17 16:19:29 +0900

    로그 추적기 version 1 완성

---
+++ src/test/java/hello/advanced/HelloTraceV1Test.java
@@ -0,0 +1,22 @@
+package hello.advanced;
+
+import hello.advanced.trace.HelloTraceV1;
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+public class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("Hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("Hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
